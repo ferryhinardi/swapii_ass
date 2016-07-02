0b7a3c2330381f6fb98d9ec62869dc180e9633f9
Ferry Hinardi <hinardi93@gmail.com>
2016-07-03 02:17:43 +0700

    commit

--- src/utils/requestHelper.js
+++ src/utils/requestHelper.js
@@ -3,6 +3,7 @@ import { SWAPI_BASE_URL } from '../constants/config';
 
 class RequestHelper {
   static request(payload) {
+  	console.log(SWAPI_BASE_URL);
     const instance = axios.create({
       baseURL: SWAPI_BASE_URL,
       timeout: 20000
