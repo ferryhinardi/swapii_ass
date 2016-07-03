b657069454c7043c96a8ed08c0592d2630e5f2a0
Ferry Hinardi <hinardi93@gmail.com>
2016-07-03 15:10:43 +0700

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
+var https = require('https');
  
 app.use(compression());
 app.use('/', express.static(__dirname + '/dist'));
@@ -8,7 +9,10 @@ app.use('/', express.static(__dirname + '/dist'));
 app.get('*', function(req, res) {
 	res.sendFile(__dirname + '/dist/index.html');
 })
- 
+
+var httpsServer = https.createServer(app);
+httpsServer.listen(8443);
+/*
 app.listen(process.env.PORT || 7000, function() {
 	console.log(`Listening on Port: ${process.env.PORT || 7000}`);
-});
+});*/
