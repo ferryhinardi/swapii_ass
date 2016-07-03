54556c1f7f4a1b8305fa85167e094d5b31ec7130
Ferry Hinardi <hinardi93@gmail.com>
2016-07-03 15:13:52 +0700

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', function(req, res) {
 })
 
 var httpsServer = https.createServer(app);
-httpsServer.listen(8443);
+httpsServer.listen(7000);
 /*
 app.listen(process.env.PORT || 7000, function() {
 	console.log(`Listening on Port: ${process.env.PORT || 7000}`);
