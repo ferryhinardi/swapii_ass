23c87faa5eefb0ef01147c71aefa0aa413819e1d
Ferry Hinardi <hinardi93@gmail.com>
2016-07-03 14:35:53 +0700

    commit

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,18 @@
-export const SWAPI_BASE_URL = 'https://swapi.co/api/';
+import url from 'url';
+
+const config = {
+	"dev": {
+		"protocol": "http",
+		"hostname": "swapi.co",
+		"port": 80,
+		"pathname": "api",
+	},
+	"prod": {
+		"protocol": "https",
+		"hostname": "swapi.co",
+		"port": 443,
+		"pathname": "api",
+	}
+}
+
+export const SWAPI_BASE_URL = url.format(config["prod"]);
